01b2908cf2ecb14b0b347a3cabebca80b9976123
ruslanashr <ruslanakyvenko@ukr.net>
2019-07-01 19:36:23 +0300

    Added some image

--- js/mobileMenu.js
+++
@@ -1,6 +0,0 @@
-function showMenu(){
-    document.querySelector(".sidebar").className="sidebar showMenu"
-}
-function hideMenu(){
-    document.querySelector(".sidebar").className="sidebar hideMenu"
-}
